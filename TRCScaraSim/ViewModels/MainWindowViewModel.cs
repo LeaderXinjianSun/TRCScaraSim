5480084bf97f07e07ce48f108ca6bc12fdab3021
sunxinjian <715054307@qq.com>
2022-01-21 17:07:01 +0800

    添加项目文件。

---
+++ TRCScaraSim/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace TRCScaraSim.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "TRCScaraSim";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
